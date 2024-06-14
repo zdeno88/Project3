fb5e56baaa1182377d6d5a8d86f63007ff0337cf
Zdenek Skaryd <zdenek.skaryd@csobpoj.cz>
2024-06-14 17:55:43 +0200

    dark and light mode

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 const nav = document.querySelector("nav");
-const icon = document.querySelector("i");
+const icon = document.querySelector(".menu-icon i");
+const switcher = document.querySelector(".switch input");
+const mode = document.querySelector(".switcher-description p");
+const modeIcone = document.querySelector(".switcher-description i");
+
+const changeClass = (removeClass, addClass, elemnt) => {
+  elemnt.classList.remove(removeClass);
+  elemnt.classList.add(addClass);
+};
 
 icon.addEventListener("click", () => {
   if (icon.classList.contains("fa-bars")) {
-    icon.classList.remove("fa-bars");
-    icon.classList.add("fa-xmark");
+    changeClass("fa-bars", "fa-xmark", icon);
     nav.style.display = "block";
   } else {
-    icon.classList.remove("fa-xmark");
-    icon.classList.add("fa-bars");
+    changeClass("fa-xmark", "fa-bars", icon);
     nav.style.display = "none";
+  }
+});
+
+switcher.addEventListener("change", (event) => {
+  if (event.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    mode.textContent = "Dark mode";
+    changeClass("fa-sun", "fa-moon", modeIcone);
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    mode.textContent = "Light mode";
+    changeClass("fa-moon", "fa-sun", modeIcone);
   }
 });
