5d8ac8521d9c24abeb5e7165a427cb3cfeb1932a
Zdenek Skaryd <zdenek.skaryd@csobpoj.cz>
2024-06-18 17:01:58 +0200

    buildings rest

--- script.js
+++ script.js
@@ -3,6 +3,24 @@ const icon = document.querySelector(".menu-icon i");
 const switcher = document.querySelector(".switch input");
 const mode = document.querySelector(".switcher-description p");
 const modeIcone = document.querySelector(".switcher-description i");
+const arrow = document.querySelector("arrow");
+const buildingsMenu = document.querySelector(".li-buildings");
+const buildings = document.querySelector("#buildings");
+const formMenu = document.querySelector(".li-form");
+const form = document.querySelector("#form");
+const email = document.querySelector(".email");
+const heslo1 = document.querySelector(".heslo1");
+const heslo2 = document.querySelector(".heslo2");
+const textPassword = document.querySelector(".textPassword");
+const textPasswordCheck = document.querySelector(
+  ".textPasswordDiv .fa-regular"
+);
+const textEmail = document.querySelector(".textEmail");
+const textEmailCheck = document.querySelector(".textEmailDiv .fa-regular");
+
+const pattern = /^([a-z\d\.\-]+)@([a-z\d\-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
+const buildingsConst = buildings.offsetTop - 70;
+const formConst = form.offsetTop - 70;
 
 const changeClass = (removeClass, addClass, elemnt) => {
   elemnt.classList.remove(removeClass);
@@ -28,5 +46,51 @@ switcher.addEventListener("change", (event) => {
     document.documentElement.setAttribute("data-theme", "light");
     mode.textContent = "Light mode";
     changeClass("fa-moon", "fa-sun", modeIcone);
+  }
+});
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY >= 20) {
+    arrow.style.visibility = "visible";
+  } else {
+    arrow.style.visibility = "hidden";
+  }
+});
+
+arrow.addEventListener("click", () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+});
+
+buildingsMenu.addEventListener("click", () => {
+  window.scrollTo({ top: buildingsConst, behavior: "smooth" });
+});
+
+formMenu.addEventListener("click", () => {
+  window.scrollTo({ top: formConst, behavior: "smooth" });
+});
+
+heslo2.addEventListener("input", () => {
+  if (heslo1.value === heslo2.value) {
+    textPassword.textContent = "Password correct";
+    textPasswordCheck.classList.add("fa-circle-check");
+    textPasswordCheck.classList.remove("fa-circle-xmark");
+  } else {
+    textPassword.textContent = "Password uncorrect";
+    textPasswordCheck.classList.add("fa-circle-xmark");
+    textPasswordCheck.classList.remove("fa-circle-check");
+  }
+});
+
+email.addEventListener("input", () => {
+  const emailInput = email.value;
+
+  if (emailInput.match(pattern)) {
+    textEmail.textContent = "Email correct";
+    textEmailCheck.classList.add("fa-circle-check");
+    textEmailCheck.classList.remove("fa-circle-xmark");
+  } else {
+    textEmail.textContent = "Email uncorrect";
+    textEmailCheck.classList.add("fa-circle-xmark");
+    textEmailCheck.classList.remove("fa-circle-check");
   }
 });
